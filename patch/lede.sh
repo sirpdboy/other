7f1ab4a50f217e3c053b6f6f645fbd76940c1750
siropboy <64370904+siropboy@users.noreply.github.com>
2020-07-01 23:26:58 +0800

    yf
    yf

--- patch/lede.sh
+++ patch/lede.sh
@@ -6,7 +6,9 @@
 red='\e[91m'
 green='\e[92m'
 yellow='\e[93m'
-echo -e "$yellow 更新必备插件库"
+echo -e "--------------------------------------------"
+echo -e "---------------更新必备插件库---------------"
+echo -e "--------------------------------------------"
 git clone https://github.com/siropboy/mypackages package/mypackages
 git clone https://github.com/siropboy/mysmall package/mysmall
 #优化feeds luci
@@ -15,19 +17,21 @@ rm -rf ./package/lean/luci-theme-rosy
 rm -rf ./package/lean/luci-theme-opentomcat
 sed -i 's/= "unreachable"/= "default"/g' feeds/luci/applications/luci-app-mwan3/luasrc/model/cbi/mwan/policyconfig.lua
 sed -i 's/带宽监控/监控/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
-wget -P  ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system https://git.io/JJJ7w
-wget -P  ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin https://git.io/JJJ7o
-wget -P package/lean/default-settings/files  https://git.io/JJJ7i
+wget -P  ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/poweroff.htm
+wget -P  ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/system.lua
+wget -P package/lean/default-settings/files  https://raw.githubusercontent.com/siropboy/other/master/patch/default-settings/zzz-default-settings
 sed -i 's/实时流量监测/监测/g' package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
 sed -i 's/网络存储/存储/g' package/lean/luci-app-vsftpd/po/zh-cn/vsftpd.po
 sed -i 's/Turbo ACC 网络加速/ACC网络加速/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
 sed -i 's/解锁网易云灰色歌曲/解锁灰色歌曲/g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
 sed -i '/filter_aaaa/d' package/network/services/dnsmasq/files/dhcp.conf
 sed -i 's/$(VERSION_DIST_SANITIZED)/$(shell date +%Y%m%d-%H%M%S)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
-wget -P package/lean/autocore/files  https://git.io/JJJ7S
-wget -P package/lean/autocore/files https://git.io/JJJ7Q
+wget -P package/lean/autocore/files  https://raw.githubusercontent.com/siropboy/other/master/patch/autocore/files/index.htm
+wget -P package/lean/autocore/files https://raw.githubusercontent.com/siropboy/other/master/patch/autocore/files/sbin/cpuinfo
 ##获取额外package
-echo -e "$red 同步更新最新额外插件"
+echo -e "--------------------------------------------------"
+echo -e "---------------同步更新最新额外插件---------------"
+echo -e "--------------------------------------------------"
 rm -rf ./package/new
 ##beardropper
 git clone https://github.com/NateLol/luci-app-beardropper package/new/luci-app-beardropper

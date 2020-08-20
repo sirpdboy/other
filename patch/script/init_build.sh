0ba4a5eba187fd1daaf9c7edd73aa10c747977bd
siropboy <64370904+siropboy@users.noreply.github.com>
2020-08-20 16:50:25 +0800

    set

--- patch/script/init_build.sh
+++ patch/script/init_build.sh
@@ -3,4 +3,4 @@ sudo -E apt-get update
 sudo -E apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs gcc-multilib g++-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler antlr3 gperf wget swig
 sudo -E apt-get -y autoremove --purge
 sudo -E apt-get clean
-sudo timedatectl set-timezone "$TZ"
+sudo timedatectl set-timezone Asia/Shanghai

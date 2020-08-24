e3a3465267a791ad2656b74edb74d3720ee0b57b
siropboy <64370904+siropboy@users.noreply.github.com>
2020-08-25 00:29:03 +0800

    mini

--- patch/script/update_package_mini.sh
+++ patch/script/update_package_mini.sh
@@ -3,13 +3,13 @@
 # Description: Build OpenWrt using GitHub Actions
 # Author: sirpdboy
 # https://github.com/sirpdboy/Openwrt
-
 rm -rf ./package/new
-rm -rf ./feeds/packages/admin/netdata/
 rm -rf ./package/lean/luci-theme-argon
 rm -rf ./package/lean/trojan
+rm -rf ./package/lean/luci-lib-docker
 rm -rf ./package/lean/luci-theme-opentomcat
 rm -rf ./package/lean/luci-app-netdata
+rm -rf ./feeds/packages/admin/netdata
 sed -i 's/= "unreachable"/= "default"/g' feeds/luci/applications/luci-app-mwan3/luasrc/model/cbi/mwan/policyconfig.lua
 sed -i 's/带宽监控/监控/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
 curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
@@ -25,18 +25,14 @@ sed -i '/filter_aaaa/d' package/network/services/dnsmasq/files/dhcp.conf
 sed -i 's/$(VERSION_DIST_SANITIZED)/$(shell date +%Y%m%d)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 curl -fsSL https://raw.githubusercontent.com/siropboy/other/master/patch/autocore/files/index.htm > package/lean/autocore/files/index.htm
 echo "DISTRIB_REVISION='S$(date +%Y.%m.%d) Sirpdboy'" > ./package/base-files/files/etc/openwrt_release1
-svn co https://github.com/siropboy/other/trunk/patch/netdata feeds/packages/admin/netdata 
-git clone https://github.com/siropboy/mypackages package/mypackages
-git clone https://github.com/siropboy/mysmall package/mysmall
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/new/luci-app-ssr-plus
-git clone -b master --single-branch https://github.com/tty228/luci-app-serverchan package/new/luci-app-serverchan
+svn co https://github.com/siropboy/other/trunk/patch/netdata feeds/packages/admin/netdata
+svn co https://github.com/siropboy/other/trunk/patch/luci-app-netdata ./package/lean/luci-app-netdata
 svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns package/new/smartdns
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/new/luci-app-smartdns
-#git clone -b master --single-branch https://github.com/destan19/OpenAppFilter package/new/OpenAppFilter
+git clone -b master --single-branch https://github.com/destan19/OpenAppFilter package/new/OpenAppFilter
 svn co https://github.com/kenzok8/openwrt-packages/trunk/adguardhome package/new/AdGuardHome
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/new/luci-app-adguardhome
 git clone -b master --single-branch https://github.com/vernesong/OpenClash package/new/openclash
-#svn co https://github.com/vernesong/OpenClash/branches/master/luci-app-openclash  package/new/luci-app-openclash
 git clone -b master --single-branch https://github.com/frainzy1477/luci-app-clash package/new/luci-app-clash
 sed -i 's/), 5)/), 48)/g' package/new/luci-app-clash/luasrc/controller/clash.lua
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/new/luci-app-passwall

77fc3c6b3845a91deea7d650b2f562702c897030
siropboy <64370904+siropboy@users.noreply.github.com>
2020-09-09 22:41:28 +0800

    https-dns-proxy
    del

--- patch/script/update_package_all.sh
+++ patch/script/update_package_all.sh
@@ -10,6 +10,7 @@ rm -rf ./package/lean/luci-lib-docker
 rm -rf ./package/lean/luci-theme-opentomcat
 rm -rf ./package/lean/luci-app-netdata
 rm -rf ./feeds/packages/admin/netdata
+rm -rf ./feeds/packages/net/https-dns-proxy
 sed -i 's/= "unreachable"/= "default"/g' feeds/luci/applications/luci-app-mwan3/luasrc/model/cbi/mwan/policyconfig.lua
 curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
 curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua

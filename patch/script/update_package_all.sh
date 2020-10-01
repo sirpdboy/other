5e4b5655622457f644528fc67ff6a3af4214f6c5
siropboy <64370904+siropboy@users.noreply.github.com>
2020-10-02 01:41:07 +0800

    set

--- patch/script/update_package_all.sh
+++ patch/script/update_package_all.sh
@@ -7,7 +7,7 @@
 rm -rf ./package/lean/luci-theme-argon
 #rm -rf ./package/lean/trojan
 #rm -rf ./package/lean/v2ray
-# rm -rf ./package/lean/luci-lib-docker
+rm -rf ./package/lean/luci-app-netdata
 rm -rf ./package/lean/luci-theme-opentomcat
 rm -rf ./feeds/diy/adguardhome
 rm -rf ./feeds/diy/smartdns
@@ -24,6 +24,7 @@ sed -i 's/Turbo ACC 网络加速/ACC网络加速/g' ./package/lean/luci-app-sfe/
 sed -i 's/解锁网易云灰色歌曲/解锁灰色歌曲/g' ./package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
 sed -i 's/家庭云//g' ./package/lean/luci-app-familycloud/luasrc/controller/familycloud.lua
 sed -i '/filter_aaaa/d' ./package/network/services/dnsmasq/files/dhcp.conf
+sed -i 's/bootstrap/opentopd/g' ./feeds/luci/collections/luci/Makefile
 # sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-ipv6-/g' ./include/image.mk
 sed -i 's/$(VERSION_DIST_SANITIZED)/$(shell date +%Y%m%d)-ipv6-/g' ./include/image.mk
 curl -fsSL https://raw.githubusercontent.com/siropboy/other/master/patch/autocore/files/index.htm > ./package/lean/autocore/files/index.htm

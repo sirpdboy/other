ee8964960ee5e6d3448bdb9d2fc7eb594773b9b9
siropboy <64370904+siropboy@users.noreply.github.com>
2020-10-03 23:15:27 +0800

    openclash

--- patch/script/uppackage.sh
+++ patch/script/uppackage.sh
@@ -5,7 +5,7 @@
 # https://github.com/sirpdboy/Openwrt
 sed -i 's/$(VERSION_DIST_SANITIZED)/$(shell date +%Y%m%d)-ipv6-/g' ./include/image.mk
 svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk/ ./package/new/luci-app-jd-dailybonus
-git clone -b master --single-branch https://github.com/vernesong/OpenClash/luci-app-openclash ./package/new/luci-app-openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash ./package/new/luci-app-openclash
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  ./package/new/luci-app-vssr
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall ./package/new/luci-app-passwall
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package ./package/package

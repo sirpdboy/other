17f3db985a19d2dcf7932c819aec571ddc57325e
siropboy <64370904+siropboy@users.noreply.github.com>
2020-10-04 00:19:23 +0800

    set/

--- patch/script/uppackage.sh
+++ patch/script/uppackage.sh
@@ -5,7 +5,7 @@
 # https://github.com/sirpdboy/Openwrt
 sed -i 's/$(VERSION_DIST_SANITIZED)/$(shell date +%Y%m%d)-ipv6-/g' include/image.mk
 svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk/ ./package/new/luci-app-jd-dailybonus
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash ./package/new/luci-app-openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash/ ./package/new/luci-app-openclash
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  ./package/new/luci-app-vssr
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall ./package/new/luci-app-passwall
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package ./package/package
@@ -14,4 +14,4 @@ sed -i 's/), 5)/), 49)/g' package/new/luci-app-clash/luasrc/controller/clash.lua
 sed -i 's/), 1)/), 49)/g' package/new/luci-app-clash/luasrc/controller/clash.lua
 git clone -b master --single-branch https://github.com/tty228/luci-app-serverchan ./package/new/luci-app-serverchan
 git clone -b master --single-branch https://github.com/destan19/OpenAppFilter ./package/new/OpenAppFilter
-svn co https://github.com/siropboy/siropboy-package/trunk  ./package/oppackpage
+svn co https://github.com/siropboy/siropboy-package/trunk/  ./package/oppackpage

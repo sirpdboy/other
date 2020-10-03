1a39c345df60a23b4f88c96d9ca7f885f98e41dc
siropboy <64370904+siropboy@users.noreply.github.com>
2020-10-03 23:21:45 +0800

    set

--- patch/script/initfeed.sh
+++ patch/script/initfeed.sh
@@ -29,11 +29,11 @@ rm -rf ./feeds/luci/applications/luci-app-nlbwmon
 #sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
 #sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
-sed -i "s/bootstrap/$default_theme/g" ./feeds/luci/modules/luci-base/root/etc/config/luci
-sed -i 's/bootstrap/$default_theme/g' ./feeds/luci/collections/luci/Makefile
-sed -i '/filter_/d' ./package/network/services/dnsmasq/files/dhcp.conf
+sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+sed -i 's/bootstrap/$default_theme/g' feeds/luci/collections/luci/Makefile
+sed -i '/filter_/d' package/network/services/dnsmasq/files/dhcp.conf
 curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
 curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
 curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/default-settings/zzz-default-settings > ./package/lean/default-settings/files/zzz-default-settings
-echo "DISTRIB_REVISION='S$(date +%Y.%m.%d) Sirpdboy'" > ./package/base-files/files/etc/openwrt_release1
+echo "DISTRIB_REVISION='S$(date +%Y.%m.%d) Sirpdboy'" > package/base-files/files/etc/openwrt_release1
 ./scripts/feeds update -i

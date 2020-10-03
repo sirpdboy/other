b38b24de5d2b65080d2d176e690e2da12defc3c9
siropboy <64370904+siropboy@users.noreply.github.com>
2020-10-03 21:03:02 +0800

    lua-maxminddb

--- patch/script/uppackage.sh
+++ patch/script/uppackage.sh
@@ -7,7 +7,6 @@ sed -i 's/$(VERSION_DIST_SANITIZED)/$(shell date +%Y%m%d)-ipv6-/g' ./include/ima
 svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk/ ./package/new/luci-app-jd-dailybonus
 git clone -b master --single-branch https://github.com/vernesong/OpenClash/luci-app-openclash ./package/new/luci-app-openclash
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  ./package/new/luci-app-vssr
-svn co https://github.com/siropboy/mypackages/trunk/lua-maxminddb ./package/new/lua-maxminddb
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall ./package/new/luci-app-passwall
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package ./package/package
 git clone -b master --single-branch https://github.com/frainzy1477/luci-app-clash ./package/new/luci-app-clash

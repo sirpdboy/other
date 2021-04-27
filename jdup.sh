c6a92e99321d141986178c4a9c17448e9622529c
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2021-04-27 10:48:12 +0800

    jd

---
+++ jdup.sh
@@ -0,0 +1,9 @@
+opkg update
+
+opkg install libnghttp214 git git-http node node-npm openssh-client openssh-keygen python3
+
+cd /etc/init.d
+
+wget https://raw.githubusercontent.com/sirpdboy/other/main/jd_openwrt_script -O jd_openwrt_script && chmod +x jd_openwrt_script
+
+/etc/init.d/jd_openwrt_script start
